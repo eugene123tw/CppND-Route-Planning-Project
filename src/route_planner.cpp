dea2dcb9e1fb92764c3ff8f0d5bdc22cc22cf5a5
劉侑群 <eugene@ryuuyuuguns-mbp.lan>
2021-08-29 07:43:02 +0100

    remove unused header

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,6 +1,5 @@
 #include "route_planner.h"
 #include <algorithm>
-#include <iostream>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y,
                            float end_x, float end_y)
